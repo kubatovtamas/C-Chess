f11c4618f96a200cb8015c3b4234842a5bac3873
igdra <pg3193@gmail.com>
2020-06-12 13:54:55 +0200

    Main loop outline

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include<wchar.h>
 #include<locale.h>
 
+int inmenu = 1;
+int playing = 0;
+int inp = 0;
+
 int main() {
     // pseudo-code:
     // while inmenu
@@ -14,10 +18,33 @@ int main() {
     //      3. Quit
     // while playing
     //      GetNext Step
+    // - Gigi
+   while (inmenu) {
+        printf("1. New Game\n");
+        printf("2. Load Game\n");
+        printf("3. Quit\n");
+        printf("\n");
+        scanf("%d", &inp);
+
+        switch (inp) {
+            case 1: printf("Starting...\n"); break;
+            case 2: printf("Loading...\n"); break;
+            case 3: {
+                printf("Goodbye!\n");
+                inmenu = 0;
+                break;
+            }
+            default: printf("I'm sorry, what was that?\n");
+                break;
+        }
+   }
+
     setlocale(LC_CTYPE, "");
     for (int i = 0; i < 12; ++i) {
         wchar_t ch = (0x2654 + i);
         wprintf(L"%lc\n", ch);
     }
+
+
     return 0;
 }
