b77ead24f43b495c2c9c206a83f45f5f3b22e9d2
kubatovtamas <kubatovtamas01@gmail.com>
2020-06-12 11:46:33 +0200

    Print chess pieces to console.

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 //
 // Created by kuba on 6/12/20.
 //
-#include <stdio.h>
+#include<stdio.h>
+#include<wchar.h>
+#include<locale.h>
 
 int main() {
-    printf("meh\n");
+    setlocale(LC_CTYPE, "");
+    for (int i = 0; i < 12; ++i) {
+        wchar_t ch = (0x2654 + i);
+        wprintf(L"%lc\n", ch);
+    }
     return 0;
 }
