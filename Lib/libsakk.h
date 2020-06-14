29504072ea78a213cf2a98ca3b09f84f7d247624
igdra <pg3193@gmail.com>
2020-06-14 12:50:25 +0200

    Added typedefs, uppercased constant enums

--- Lib/libsakk.h
+++ Lib/libsakk.h
@@ -8,7 +8,7 @@
 #include<wchar.h>
 
 // Pieces - Gigi
-enum PieceType {
+enum PIECE_TYPE {
  WHITEQUEEN = 0x2654,
  WHITEKING = 0x2655,
  WHITEROOK = 0x2656,
@@ -23,50 +23,60 @@ enum PieceType {
  BLACKKNIGHT = 0x265E,
  BLACKPAWN = 0x265F
 };
+typedef enum PIECE_TYPE PIECE_TYPE;
 
-enum Color {WHITE, BLACK};  // white 1, black 0 (e.g. if white, go first)
+// color constants by Gigi
+// white 1, black 0 (e.g. if white, go first)
+enum COLOR {WHITE, BLACK};
+typedef enum COLOR COLOR;
 
 // State structure implemented as a Doubly-linked list (idea by Kuba)
 // A Round holds a pointer to the next round (init NULL) and the previous Round.
 // The Step struct holds the data for the moves of the current round.
-// Outline implementation by Gigi
+// Outline implementation and modification by Gigi
 struct State {
     // save state changes here (before,after)
     int key;                  // 0 default state, each step increments by one
 
-    struct Step* step;
-    struct State* previous;
-    struct State* next;
+    Step* step;
+    State* previous;
+    State* next;
 };
+typedef struct State State;
 extern const struct State *defaultstate;                 // this link always point to first Link
 extern struct State *laststate;                          // this link always point to last Link
 
 // Step struct idea and pseudocode by Kuba
+// Modified by Gigi
 struct Step {
     //tiles: [C2, C3];            // Array of Tiles, min:2(normal), max: 4(castling)
     //before: [WHITEPAWN, NULL];  // Array of Pieces
     //after: [NULL, WHITEPAWN];   // Array of Pieces
-    // TODO tile implementation
-    //  tiles[4];
-    wchar_t before[2];
-    wchar_t after[2];
+    // TODO add parameters for castling
+
+    Piece before;                   // moved piece before move
+    Piece after;                    // after move (info new position and if promoted)
+    Piece hit;                      // piece that was hit and moved out of play
 };
+typedef struct Step Step;
 
 void step();                                             // change state
 
 // bool pieceHasMoved idea by Kuba
-// struct alternative by Gigi
+// struct Piece and struct Position alternative idea by Gigi
 struct Piece {
-    enum PieceType type;               // stores what type of piece
-    enum Color color;                   // piece Color
+    PIECE_TYPE type;               // stores what type of piece
+    COLOR color;                   // piece Color
     _Bool hasmoved;                     // has the piece moved so far in the game
-    struct Position pos;                // x and y coordinates of piece location
+    Position pos;                // x and y coordinates of piece location
 };
+typedef struct Piece Piece;
 
 struct Position{
     int x;
     int y;
 };
+typedef struct Position Position;
 
 // This is just an outline for planned features
 // TODO Proper definitions
