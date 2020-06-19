4676bb4f2ad0017df08dca991e4342dda9f9fe5d
igdra <pg3193@gmail.com>
2020-06-19 18:13:37 +0200

    game_start removed from test_game_start

--- tst/utest-game.c
+++ tst/utest-game.c
@@ -7,7 +7,6 @@
 Game* game = NULL;
 
 void test_game_start() {
-    game = game_start();
     CU_ASSERT_PTR_NOT_NULL(game);
     CU_ASSERT_PTR_EQUAL(displayed_game_state_ptr, game->currentstate);
 }
